fc011520ef52199e78893d0be57e43170ca8db4d
hdanielrc <daniel.rojas@rockdrillgroup.com>
2025-10-31 19:41:04 -0500

    version2

--- venv/Scripts/runxlrd.py
+++ venv/Scripts/runxlrd.py
@@ -1,4 +1,4 @@
-#!C:\Users\PERDLAP140.VILBRAGROUP\Desktop\rdapp\venv\Scripts\python.exe
+#!C:\Users\danie\OneDrive\Escritorio\despliegue\aplicativo\venv\Scripts\python.exe
 # Copyright (c) 2005-2012 Stephen John Machin, Lingfo Pty Ltd
 # This script is part of the xlrd package, which is released under a
 # BSD-style licence.
